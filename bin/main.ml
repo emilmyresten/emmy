4160c87040dfca37fb7c6636045e8d2940537d84
Emil Myresten <emil.myresten@lansforsakringar.se>
2023-11-26 21:33:02 +0100

    add repl start time on init

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,5 @@
 open Interp 
+open Unix
 
 let read () = Stdlib.read_line ()
 
@@ -12,5 +13,10 @@ let rec loop ctx =
     loop ctx
   with e -> print_endline (Printexc.to_string e); loop ctx
 
-let () = let ctx = [] in loop ctx
+let () = let ctx = [] in 
+  let t = Unix.localtime (Unix.time ()) in
+  let with_leading_zero n = if n < 10 then Printf.sprintf "0%d" n else Printf.sprintf "%d" n in
+  let formatted_time = Printf.sprintf "%s:%s:%s" (with_leading_zero t.tm_hour) (with_leading_zero t.tm_min) (with_leading_zero t.tm_sec) in
+  Printf.printf "REPL started at %s\n" formatted_time;
+  loop ctx
   
